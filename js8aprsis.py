bb1181eb6fab8a5853a136c4a461570133f980e7
Kaos <kaos@iMac.local>
2019-12-10 14:37:53 -0800

    Flesh out APRS-IS gateway, make it check that callsign is seen on JS8 and then transmit.
    Currently only supports MSG

--- js8aprsis.py
+++ js8aprsis.py
@@ -4,6 +4,7 @@ import socket
 import aprs
 import time
 import json 
+import sys
 
 #config goes here
 aprs_pass = "17574"
@@ -15,20 +16,52 @@ sock.bind(("",js8port))
 
 def callback(x):
   print(x)
-  
+  try:
+   fromcall = str(x).split(":")[2]
+  except: return
+  try:
+   targetcall = str(x).split(":")[3]
+  except: return
+  try:
+   sendmsg = str(x).split(":")[4]
+  except: return
+
+  print("From Call:" + fromcall)
+  print("Target callsign:" + targetcall)
+  print("Outgoing message:" + sendmsg)
+  jsonout= "{\"params\": {\"_ID\": "+str(int(time.time()*1000))+"}, \"type\": \"RX.GET_CALL_ACTIVITY\", \"value\": \"\"}"
+  sock.sendto(bytes(jsonout,"utf8"), js8sock)
+  rec = sock.recv(1024)
+  pingtest = str(rec)
+#  print("pingtest:" + pingtest)
+  if (pingtest.find("PING") != -1): 
+      print("PING!!")
+      rec = sock.recv(1024)
+  json_object = json.loads(rec)
+  if (str(json_object).find(targetcall) !=-1): 
+      print("FOUND CALL! WOO!")
+      js8string = targetcall + " MSG" + sendmsg
+      print("Final out:" + js8string.rstrip())
+      jsonout= "{\"params\": {\"_ID\": "+str(int(time.time()*1000))+"}, \"type\": \"TX.SEND_MESSAGE\", \"value\": \"" + js8string.rstrip() + "\"}"
+      print("Jsonout:" + jsonout)
+      print("Sending to JS8!")
+      sock.sendto(bytes(jsonout,"utf8"), js8sock)
+  #print("rec:" + str(json_object))  
 
 print("APRS-IS to JS8 Gateway. V0.1")
 print("Waiting for JS8 to connect...")
 rec, js8sock = sock.recvfrom(1024)
 print ("JS8 Connected " + str(js8sock))
+#Get Callsign from JS8
 jsonout= "{\"params\": {\"_ID\": "+str(int(time.time()*1000))+"}, \"type\": \"STATION.GET_CALLSIGN\", \"value\": \"\"}"
 sock.sendto(bytes(jsonout,"utf8"), js8sock)
 rec = sock.recv(1024)
 json_object = json.loads(rec)
 print ("foo:"+ json_object['value'])
+callsign=json_object['value']
 
 print("Connecting to APRS-IS...")
-aprs = aprs.TCP(bytes(json_object['value'], 'utf8'),aprs_pass,False,"t/m")
+aprs = aprs.TCP(bytes(callsign, 'utf8'),aprs_pass,False,"d/JS8*")
 aprs.start()
 aprs.receive(callback=callback)
 
