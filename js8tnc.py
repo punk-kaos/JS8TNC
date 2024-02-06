c3e91f9a86bb7dd0c5ef2270ba98e479848302e3
Kelly-Shane Fuller <kfuller@dstsystems.com>
2024-02-06 15:04:11 -0800

    overhaul the TNC to work with current JS8Call API.

--- js8tnc.py
+++ js8tnc.py
@@ -1,42 +1,67 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 import socket
 import time
 import json
 
-port = 2242
+# Define ports
 kiss_port = 8001
+print("JS8 KISS TNC V2BETA")
+# Create TCP socket for KISS TNC
+kiss_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+kiss_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+kiss_socket.bind(("0.0.0.0", kiss_port))
+kiss_socket.listen(1)
+print("TNC started on port", kiss_port)
+print("Waiting for TNC client to connect...")
 
-kiss = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(("0.0.0.0",port))
-print ("JS8Call TNC V0.2")
-print ("Waiting for JS8 to connect...")
-rec, js8sock = sock.recvfrom(1024)
-print ("JS8 Connected " + str(js8sock))
-kiss.bind(("", kiss_port))
-#kiss.setblocking(False)
-kiss.listen(1)
-print ("TNC started on port " + str(kiss_port))
-print ("Waiting for TNC client to connect...")
-conn, addr = kiss.accept()
-print ('TNC Client connected from: ', addr)
+# Wait for KISS TNC client connection
+kiss_connection, kiss_address = kiss_socket.accept()
+print('TNC Client connected from:', kiss_address)
 
 
+def send_tcp(msg):
+
+    msg = msg + "\n"
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        try:
+            s.connect(("127.0.0.1",2442))
+            print("Connected")
+            s.sendall(msg.encode())
+            print(f"Sent to JS8: {msg}")
+        finally:
+            s.close()
+# Main loop
 while True:
+    print("KISS loop")
+    kiss_data = kiss_connection.recv(1024)
+    if not kiss_data:
+        break
 
-   print ("KISS loop")
-   kiss_in = conn.recv(1024)
-   if not len(kiss_in) >0: break
-   print (kiss_in.decode("utf8","ignore"))
+    # Process KISS frame
+    try:
+        # Extract message
 
-   try:
-     print (kiss_in.decode("utf8","ignore").split('bbnc')[1])
-   except:
-     print ("Error decoding frame.")
-     continue  
-   
-   js8string = "@APRSIS CMD " + kiss_in.decode("utf8","ignore").split('bbnc')[1]
-   jsonout= "{\"params\": {\"_ID\": "+str(int(time.time()*1000))+"}, \"type\": \"TX.SEND_MESSAGE\", \"value\": \"" + js8string.rstrip() + "\"}"
-   print (jsonout)
-   sock.sendto(bytes(jsonout,"utf8"), js8sock)
+        message = kiss_data.decode('utf-8', errors='ignore').split("\u0003")[1]
+        print("Extracted message:", message)
+
+        # Construct JSON message for JS8Call
+        json_message = {
+            "params": {},
+            "type": "TX.SEND_MESSAGE",
+            "value": "@APRSIS CMD " + message
+        }
+        json_string = json.dumps(json_message)
+        print("JSON message:", json_string)
+        send_tcp(json_string)
+
+
+    except Exception as e:
+        print("Error processing KISS frame:", e)
+        continue
+
+# Close sockets
+kiss_connection.close()
+kiss_socket.close()
+
